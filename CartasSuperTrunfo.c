0f093899f3e69bf289051da946d19a654bbb7219
pedrochagas004-droid <pedrochagas004@gmail.com>
2026-01-25 21:08:03 +0000

    Estrutura 1 Primeira cidade

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -5,8 +5,40 @@
 // Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
 
 int main() {
-  printf("Desafio Xadrez!\n");
+ #include <stdio.h>
+
+int main() {
+    // Variáveis para a primeira carta
+    char estado;              // Ex: 'A'
+    char codigo[4];           // Ex: "A01"
+    char nomeCidade[50];      // Ex: "Rio de Janeiro"
+    int populacao;            // Números inteiros
+    float area;               // Números com vírgula (km²)
+    float pib;                // Números com vírgula (Dinheiro)
+    int pontosTuristicos;     // Números inteiros]
+
+    printf("Digite a letra do Estado (A-H): ");
+    scanf(" %c", &estado);
+
+    printf("Digite o codigo da carta (ex: A01): ");
+    scanf("%s", codigo);
+
+    printf("Digite o nome da cidade: ");
+    scanf(" %[^\n]s", nomeCidade); // Esse comando estranho lê nomes com espaços!
+
+    printf("Populacao: ");
+    scanf("%d", &populacao);
+
+    printf("Area (km2): ");
+    scanf("%f", &area);
+
+    printf("PIB: ");
+    scanf("%f", &pib);
+
+    printf("Pontos turisticos: ");
+    scanf("%d", &pontosTuristicos);
 }
+
 
   // Área para entrada de dados
 
