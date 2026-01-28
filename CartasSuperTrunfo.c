daa463ac24fac37a1638e727fc7feabee03cdb7e
pedrochagas004-droid <pedrochagas004@gmail.com>
2026-01-28 02:55:04 +0000

    densidade e pib nivel aventureiro concluido

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -3,27 +3,23 @@
 int main() {
     // --- VARIÁVEIS CARTA 1 ---
     char estado1, codigo1[5], nomeCidade1[50], nome1[50];
-    int populacao1;
-    float area1, pib1;
-    int pontosTuristicos1;
+    int populacao1, pontosTuristicos1;
+    float area1, pib1, densidade1, pibCapita1;
 
     // --- VARIÁVEIS CARTA 2 ---
     char estado2, codigo2[5], nomeCidade2[50], nome2[50];
-    int populacao2;
-    float area2, pib2;
-    int pontosTuristicos2;
+    int populacao2, pontosTuristicos2;
+    float area2, pib2, densidade2, pibCapita2;
 
     // --- VARIÁVEIS CARTA 3 ---
     char estado3, codigo3[5], nomeCidade3[50], nome3[50];
-    int populacao3;
-    float area3, pib3;
-    int pontosTuristicos3;
+    int populacao3, pontosTuristicos3;
+    float area3, pib3, densidade3, pibCapita3;
 
-     // --- VARIÁVEIS CARTA 4 ---
+    // --- VARIÁVEIS CARTA 4 ---
     char estado4, codigo4[5], nomeCidade4[50], nome4[50];
-    int populacao4;
-    float area4, pib4;
-    int pontosTuristicos4;
+    int populacao4, pontosTuristicos4;
+    float area4, pib4, densidade4, pibCapita4;
 
     // --- CADASTRO DA CARTA 1 ---
     printf("--- CADASTRO DA CARTA 1 ---\n");
@@ -123,18 +119,30 @@ int main() {
     printf("Pontos Turisticos: %d\n", pontosTuristicos4);
 
     // --- CÁLCULOS ---
-    densidade1 = (float)pop1 / area1; pibCapita1 = (pib1 * 1000000000) / (float)pop1;
-    densidade2 = (float)pop2 / area2; pibCapita2 = (pib2 * 1000000000) / (float)pop2;
-    densidade3 = (float)pop3 / area3; pibCapita3 = (pib3 * 1000000000) / (float)pop3;
-    densidade4 = (float)pop4 / area4; pibCapita4 = (pib4 * 1000000000) / (float)pop4;
+    densidade1 = (float)populacao1 / area1;
+    pibCapita1 = (pib1 * 1000000000) / (float)populacao1;
+
+    densidade2 = (float)populacao2 / area2;
+    pibCapita2 = (pib2 * 1000000000) / (float)populacao2;
+
+    densidade3 = (float)populacao3 / area3;
+    pibCapita3 = (pib3 * 1000000000) / (float)populacao3;
+
+    densidade4 = (float)populacao4 / area4;
+    pibCapita4 = (pib4 * 1000000000) / (float)populacao4;
 
     // --- EXIBIÇÃO ---
-    printf("\n--- RESULTADO: SUPER TRUNFO ---\n");
-    
-    printf("\nCARTA 1: %s (%s)\nDensidade: %.2f hab/km2 | PIB per Capita: R$ %.2f\n", nome1, codigo1, densidade1, pibCapita1);
-    printf("CARTA 2: %s (%s)\nDensidade: %.2f hab/km2 | PIB per Capita: R$ %.2f\n", nome2, codigo2, densidade2, pibCapita2);
-    printf("CARTA 3: %s (%s)\nDensidade: %.2f hab/km2 | PIB per Capita: R$ %.2f\n", nome3, codigo3, densidade3, pibCapita3);
-    printf("CARTA 4: %s (%s)\nDensidade: %.2f hab/km2 | PIB per Capita: R$ %.2f\n", nome4, codigo4, densidade4, pibCapita4);
+    printf("\nCARTA 1: %s (%s)\n", nomeCidade1, codigo1);
+    printf("Densidade: %.2f hab/km2 | PIB por Capital: R$ %.2f\n", densidade1, pibCapita1);
+
+    printf("\nCARTA 2: %s (%s)\n", nomeCidade2, codigo2);
+    printf("Densidade: %.2f hab/km2 | PIB por Capital: R$ %.2f\n", densidade2, pibCapita2);
+
+    printf("\nCARTA 3: %s (%s)\n", nomeCidade3, codigo3);
+    printf("Densidade: %.2f hab/km2 | PIB por Capital: R$ %.2f\n", densidade3, pibCapita3);
+
+    printf("\nCARTA 4: %s (%s)\n", nomeCidade4, codigo4);
+    printf("Densidade: %.2f hab/km2 | PIB por Capital: R$ %.2f\n", densidade4, pibCapita4);
 
     return 0;
 }
